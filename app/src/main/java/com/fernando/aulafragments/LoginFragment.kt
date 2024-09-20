59eb4699c0126e0f356509c76d49170091cffe0f
Fernando Pinéo <pf1963@fiap.com.br>
2024-09-20 09:49:54 -0300

    Criação do Login

--- app/src/main/java/com/fernando/aulafragments/LoginFragment.kt
+++ app/src/main/java/com/fernando/aulafragments/LoginFragment.kt
@@ -1,34 +1,20 @@
 package com.fernando.aulafragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
+class LoginFragment : Fragment() {
 
-class CadastroFragment : Fragment() {
-    private lateinit var btnEnviar:Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_cadastro, container, false)
-
-        btnEnviar = view.findViewById(R.id.btnEnviar)
-
-        btnEnviar.setOnClickListener{
-            Log.i("teste","Clicado")
-        }
-
-
-
-
-       return view
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 }
--- app/src/main/java/com/fernando/aulafragments/MainActivity.kt
+++ app/src/main/java/com/fernando/aulafragments/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnCadastro:Button
+    private lateinit var btnLogin:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +17,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCadastro = findViewById(R.id.btnCadastro)
+        btnLogin = findViewById(R.id.btnLogin)
 
         btnCadastro.setOnClickListener{
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainerView,CadastroFragment())
                 .commit()
         }
+
+        btnLogin.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView,LoginFragment())
+                .commit()
+        }
+
+
 
 
     }
