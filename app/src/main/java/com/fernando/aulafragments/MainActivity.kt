170949a012cedb81de5bbcd515cbdee201b8c467
Fernando Pinéo <pf1963@fiap.com.br>
2024-09-20 09:26:04 -0300

    Aula - Resolução proglema do Camarinha

---
+++ app/src/main/java/com/fernando/aulafragments/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.fernando.aulafragments
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnCadastro:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnCadastro = findViewById(R.id.btnCadastro)
+
+        btnCadastro.setOnClickListener{
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView,CadastroFragment())
+                .commit()
+        }
+
+
+    }
+}
